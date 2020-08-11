f29d0f860f2f46c97f1b86a86b32b42e58b67d9d
jurfed <jurfed@inbox.ru>
2020-08-11 16:13:40 +0300

    add junit test

--- src/test/java/ru/jurfed/lottery/LotteryServerApplicationTests.java
+++ src/test/java/ru/jurfed/lottery/LotteryServerApplicationTests.java
@@ -1,13 +1,41 @@
 package ru.jurfed.lottery;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LotteryServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@LocalServerPort
+	int randomServerPort;
+
+	@Test
+	public void testGetEmployeeListSuccess() throws URISyntaxException
+	{
+		RestTemplate restTemplate = new RestTemplate();
+
+		final String baseUrl = "http://localhost:" + randomServerPort + "/";
+		URI uri = new URI(baseUrl);
+
+		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+		//Verify request succeed
+		Assert.assertEquals(200, result.getStatusCodeValue());
+		Assert.assertEquals(true, result.getBody().contains("Games"));
 	}
 
 }
